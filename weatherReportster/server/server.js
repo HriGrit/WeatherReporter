96291443a2848192fb60af9040be385962136935
HriGrit <86262322+HriGrit@users.noreply.github.com>
2023-11-16 23:39:54 +0530

    yes

--- weatherReportster/server/server.js
+++ weatherReportster/server/server.js
@@ -7,6 +7,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+	res.send("Hello from server!");
+});
+
 app.get("/api", (req, res) => {
 	res.json({ message: "Hello from server!" });
+});
+
+app.listen("https://weather-reporter-rho.vercel.app/", () => {
+	console.log("Server running on port 5000");
 });

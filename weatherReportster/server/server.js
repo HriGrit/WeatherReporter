1affbccaa89182e9e84f7d5478d38e3ea89d3c75
HriGrit <86262322+HriGrit@users.noreply.github.com>
2023-11-17 11:27:48 +0530

    work

--- weatherReportster/server/server.js
+++ weatherReportster/server/server.js
@@ -3,8 +3,6 @@ const app = express();
 const axios = require("axios");
 const cors = require("cors");
 
-require("dotenv").config();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
